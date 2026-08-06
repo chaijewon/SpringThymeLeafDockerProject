dfa5093c1a89cc5e86eb06525185d4700bfe666d
chaijewon <vcandjava@nate.com>
2026-08-06 12:47:18 +0900

    1111

--- src/main/java/com/sist/web/SpringThymeLeafDockerProjectApplication.java
+++ src/main/java/com/sist/web/SpringThymeLeafDockerProjectApplication.java
@@ -2,7 +2,19 @@ package com.sist.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/*
+ *    git init ==> 연결하는 폴더 생성 
+ *    git remote add origin git 주소 (git 연결)
+ *    git add .  => 보낼 데이터 모우기 
+ *    git commit -m ""
+ *    git push -u origin master --force 
+ *                          => 강제 전송 
+ *    ---------------------------------------
+ *    수정
+ *    git add .
+ *    git commit -m ""
+ *    git push -u origin master
+ */
 @SpringBootApplication
 public class SpringThymeLeafDockerProjectApplication {
 
